9eaa92d122e041eea4225fc7eeb549dabc89f294
SridharanManoharan <shrisridharan@yahoo.in>
2015-12-16 11:10:12 +0530

    Second Project Protocol 1

---
+++ ProjectBootstrap/includes/js/script.js
@@ -0,0 +1,11 @@
+//Menu Toggle Script
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+$(function() {
+
+	$('[rel="tooltip"]').tooltip();
+
+});
